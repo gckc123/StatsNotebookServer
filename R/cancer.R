f0cca9910db5b6d55d0e5c01079f62a715381b91
gckc123 <gckc123@gmail.com>
2020-09-20 12:17:03 +1000

    update cancer dataset 20.9.2020

---
+++ R/cancer.R
@@ -0,0 +1,24 @@
+#' A simulated dataset based on the simulated cancer dataset from UCLA Institute for
+#' Digital Research and Education
+#'
+#' Lung cancer data from 5959 patients
+#'
+#'
+#' @format A data frame with 5959 rows and 12 variables:
+#' \describe{
+#'   \item{pain}{Pain level ranged from 1 to 9}
+#'   \item{remission}{1: Remitted; 0: Not remitted}
+#'   \item{Age}{Patient age}
+#'   \item{SmokingHx}{Smoking status: former smoker/ current smoker/ non-smoker}
+#'   \item{Sex}{Patient sex: male/ female}
+#'   \item{CancerStage}{Cancer Stage: I/ II/ III/ IV}
+#'   \item{LengthofStay}{Length of hospital stay}
+#'   \item{BMI}{Body Mass Index}
+#'   \item{DID}{Doctor ID}
+#'   \item{Experience}{Doctor's years of experience}
+#'   \item{HID}{Hospital ID}
+#'   \item{TumourSize}{Tumor Size}
+#'
+#' }
+#' @source \url{https://stats.idre.ucla.edu/r/dae/mixed-effects-logistic-regression/}
+"cancer"
