7cab47b38c399bd5e279f9acebb5a1f3a7ebeac4
gckc123 <gckc123@gmail.com>
2020-09-24 12:30:54 +1000

    add wellbeing example data 24.9.2020

---
+++ R/wellbeing.R
@@ -0,0 +1,15 @@
+#' A simulated dataset on wellbeing, stress and social support
+#'
+#' A simulated dataset of 1000 participants
+#'
+#'
+#' @format A data frame with 231 rows and 9 variables:
+#' \describe{
+#'   \item{stress}{level of stress}
+#'   \item{SocialSupport}{Level of social support}
+#'   \item{Wellbeing}{Level of psychological wellbeing}
+#'   \item{Sex}{Male/ Female}
+#'
+#' }
+
+"wellbeing"
