b4f74fe3122bc15ac0ec8b9a38fe959ea50964e6
gckc123 <gckc123@gmail.com>
2020-09-25 15:46:27 +1000

    add stars data 25.9.2020

---
+++ R/stars.R
@@ -0,0 +1,14 @@
+#' A dataset of star temperature and light intensity
+#'
+#' This dataset is sourced from the robustbase package, originally named StarsCYG.
+#' It contains 47 observations of star temperature and light intensity.
+#'
+#'
+#' @format A data frame 47 observations and 2 variabless:
+#' \describe{
+#'   \item{temperature}{Surface temperature of a star}
+#'   \item{light}{light intensity}
+#'
+#' }
+
+"stars"
