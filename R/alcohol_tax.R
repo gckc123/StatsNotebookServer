bea2f110056a5d00c45658fa400806127f4d8e35
gckc123 <gckc123@gmail.com>
2020-09-26 18:26:10 +1000

    add sleep data 26.9.2020

---
+++ R/alcohol_tax.R
@@ -0,0 +1,16 @@
+#' A simulated dataset based on the epi.bfi dataset from the psychTools package
+#'
+#' Alcohol consumption data from 3666 participants
+#'
+#'
+#' @format A data frame with 3666 rows and 5 variables:
+#' \describe{
+#'   \item{alcohol}{Number of standard drinks consumed in a month}
+#'   \item{Year}{Year when the data was collected}
+#'   \item{Postcode}{Postcode of the participant}
+#'   \item{State}{State}
+#'   \item{Remoteness}{Captial city or regional area}
+#'
+#' }
+
+"alcohol_tax"
---
+++ R/sleep.R
@@ -0,0 +1,18 @@
+#' Reaction times in sleep deprivation study
+#'
+#' This dataset is sourced from the lme4 package, originally named 'sleepstudy'.
+#' The average reaction time per day for subjects in a sleep deprivation study.
+#' On day 0 the subjects had their normal amount of sleep.
+#' Starting that night they were restricted to 3 hours of sleep per night.
+#' The observations represent the average reaction time on a series of tests given each day to each subject.
+#'
+#'
+#' @format A data frame with 180 observations.
+#' \describe{
+#'   \item{Reaction}{Number of standard drinks consumed in a month}
+#'   \item{Days}{Year when the data was collected}
+#'   \item{Subject}{Subject number on which the observation was made.}
+#'
+#' }
+
+"sleep"
