49770c4f87df2ed741305d7c2d10ae26d4084058
gckc123 <gckc123@gmail.com>
2020-10-24 19:14:58 +1000

    add UNDP data 24.10.2020

---
+++ R/UNDP.R
@@ -0,0 +1,22 @@
+#' A Dataset from the United Nations Development Programme
+#'
+#' A dataset of 199 countries
+#'
+#'
+#' @format A data frame with 199 rows and 11 variables:
+#' \describe{
+#'   \item{HDI_Rank_2018}{Human Development Index Ranking 2018}
+#'   \item{Country}{Country name}
+#'   \item{HDI}{Human Development Index 2018}
+#'   \item{Pop}{Population (millions) in 2018}
+#'   \item{GII}{Gender Inequality Index in 2018}
+#'   \item{CO2}{Crabon dioxide emissions, per capita (tonnes)}
+#'   \item{Prison}{Prison population (per 100,000 people) in 2017}
+#'   \item{Homocide}{Crabon dioxide emissions, per capita (tonnes)}
+#'   \item{GDP}{Crabon dioxide emissions, per capita (tonnes)}
+#'   \item{Schooling}{Crabon dioxide emissions, per capita (tonnes)}
+#'   \item{Continent}{Continent}
+#'
+#' }
+
+"UNDP"
