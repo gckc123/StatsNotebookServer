7098e2e18f8b2bf9d5c8c2c8e0f53537d68c9932
gckc123 <gckc123@gmail.com>
2020-10-27 21:34:35 +1000

    add new HDI data 27.10.2020

---
+++ R/HDI.R
@@ -0,0 +1,15 @@
+#' Human Development Index 1990-2018 by regions
+#'
+#' Human Development Index by regions from 1990 to 2018 in wide format
+#'
+#'
+#' @format A data frame with 199 rows and 11 variables:
+#' \describe{
+#'   \item{Region}{Region}
+#'   \item{HDI_1990}{Human Development Index 1990}
+#'   \item{GDP_1990}{GDP per capita 1990}
+#'
+#' }
+#' @source Data were adapted from the epi.bfi dataset from the psychTools package
+
+"HDI"
