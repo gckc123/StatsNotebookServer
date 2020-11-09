51f5946c1cf626b892bf6332eff9121c8307006a
gckc123 <gckc123@gmail.com>
2020-11-09 16:03:34 +1000

    new example data (cannabis) 9.11.2020

---
+++ R/cannabis.R
@@ -0,0 +1,24 @@
+#' Simulated data on cannabis use and future illicit substance use
+#'
+#' A simulated data on adolescent cannabis use and illicit substance use in adulthood
+#'
+#'
+#' @format A data frame with 1200 rows and 11 variables:
+#' \describe{
+#'   \item{peer_1}{Number of best friends who used drugs at Year 10. 1: None; 2: One or two of my best friends; 3: Three or more of my best friends used drugs}
+#'   \item{peer_2}{Number of best friends who used drugs at Year 11. 1: None; 2: One or two of my best friends; 3: Three or more of my best friends used drugs}
+#'   \item{peer_3}{Number of best friends who used drugs at Year 12. 1: None; 2: One or two of my best friends; 3: Three or more of my best friends used drugs}
+#'   \item{gender}{Gender. 0: Male; 1 Female}
+#'   \item{antisocial_1}{Involvement in antisocial behaviours (e.g. stealing, damaging others' properties, etc) at Year 10. 0: No; 1: Yes}
+#'   \item{antisocial_2}{Involvement in antisocial behaviours (e.g. stealing, damaging others' properties, etc) at Year 11. 0: No; 1: Yes}
+#'   \item{antisocial_3}{Involvement in antisocial behaviours (e.g. stealing, damaging others' properties, etc) at Year 12. 0: No; 1: Yes}
+#'   \item{cannabis_1}{Used cannabis at least weekly at Year 10. 0: No; 1: Yes}
+#'   \item{cannabis_2}{Used cannabis at least weekly at Year 11. 0: No; 1: Yes}
+#'   \item{cannabis_3}{Used cannabis at least weekly at Year 12. 0: No; 1: Yes}
+#'   \item{illicit_drug}{Used illicit at age 21. 0: No; 1: Yes}
+#'
+#'
+#' }
+#' @source This is a simulated dataset.
+
+"cannabis"
