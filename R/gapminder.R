e8810ba64fb970d68c4eee7c74cec8879ee05428
gckc123 <gckc123@gmail.com>
2020-12-26 09:00:06 +1000

    add gapminder data

---
+++ R/gapminder.R
@@ -0,0 +1,14 @@
+#' Life expectancy, population and GDP data from the gapminder package
+#'
+#' This dataset is sourced from the gapminder package, originally named 'sleepstudy'.
+#'
+#'
+#' @format A data frame with 1704 observations.
+#' \describe{
+#'   \item{Reaction}{Number of standard drinks consumed in a month}
+#'   \item{Days}{Year when the data was collected}
+#'   \item{Subject}{Subject number on which the observation was made.}
+#'
+#' }
+
+"gapminder"
